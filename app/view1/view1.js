16319a3c33ae605262ec7ffb750721aaeb4de080
Aloïs Prieur de La Comble <alois.prieurdelacomble@supelec.fr>
2015-09-26 12:32:03 +0200

    Réalisation du front end. Affichage d'un objet projet stocké sur la bdd MySQL DANS LA VIEW 2

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,37 @@
 'use strict';
-
 angular.module('myApp.view1', ['ngRoute'])
-
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
-
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', [ '$scope', function($scope) {
+$scope.name = '';
+$scope.names = [{name:"Chris"}, {name:"Calvin"}];
+$scope.addName = function() {
+$scope.names.push( {'name':$scope.name} );
+$scope.name = '';
+};
 }]);
+
+var services = angular.module('view1.services', ['ngResource']);
+
+services.factory('ShapeWithPropertyFactory', function ($resource) {
+    return $resource('/ShapesComputations/rest/displayshapeproperty/', {}, {
+        query: {
+            method: 'GET',
+            params: {},
+            isArray: false
+        }
+    })
+});
+
+
+
+
+//app.controller('ShapeWithPropertyCtrl', ['$scope', 'ShapeWithPropertyFactory', function ($scope, ShapeWithPropertyFactory) {
+//	ShapeWithPropertyFactory.get({}, function (ShapeWithPropertyFactory) {
+//        $scope.property = 'Bonjour monde !';
+//    })
+//}]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,14 +1,36 @@
 'use strict';
-
-angular.module('myApp.view2', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
+var application = angular.module('myApp.view2', ['ngRoute'])
+application.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
     controller: 'View2Ctrl'
   });
 }])
+application.controller('View2Ctrl', [ '$scope', function($scope) {
+$scope.name = '';
+$scope.names = [{name:"Chris"}, {name:"Calvin"}];
+$scope.addName = function() {
+$scope.names.push( {'name':$scope.name} );
+$scope.name = '';
+};
+}]);
 
-.controller('View2Ctrl', [function() {
+var services = angular.module('myApp.services', ['ngResource']);
 
+services.factory('Projects', ['$resource',
+  function($resource){
+    return $resource('http://localhost:8080/DigitalLab/rest/projects', {}, {
+      query: {
+    	  		method:'GET',
+    	  		params:{},
+    	  		isArray:false}
+    });
+  }]);
+
+application.controller('ProjectsController', ['$scope', 'Projects', function($scope, Projects) {
+	  $scope.request = Projects.query();
+	}]);
+
+application.controller('Test', ['$scope', function($scope){
+    $scope.test = "Test";
 }]);
